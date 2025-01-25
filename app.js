a5272d5c6bf7b13b00b941ac0c655a2db9d5f37e
Debora Makoski <deborahmakoski@gmail.com>
2025-01-25 16:36:17 -0300

    alterando limite jogo para 100

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ let numeroSecreto = gerarNumeroAleatorio();
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 exibirMensagemInicial() ;
 
